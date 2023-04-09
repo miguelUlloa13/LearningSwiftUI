7d587020eaad97c43442314b2378dc81ef37d0d6
Miguel <miguel.bmangelulloa13@gmail.com>
2023-04-08 20:37:54 -0600

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Miguel Angel Bric Ulloa on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
